c0441adaab26bc0d858aa79014280d06f1b35ada
HarmonicaToolkit <rick@rickhardy.com>
2022-09-30 19:42:26 +0100

    Initial Commit

---
+++ TunerEnvironmentObject/TunerEnvironmentObjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  TunerEnvironmentObjectApp.swift
+//  TunerEnvironmentObject
+//
+//  Created by Richard Hardy on 30/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct TunerEnvironmentObjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
